3bf9a39a3b48a1a49621697ea6c8b11ed2f41b28
Yaroslav Isaenko <21yroslav10@mail.ru>
2025-04-18 14:48:53 +0300

    second update, h 2.5.

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bb
+from .models import Rubric
+
+class BdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+
+admin.site.register(Bb, BdAdmin)
+admin.site.register(Rubric)
